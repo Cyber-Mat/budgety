a59ede0b79f2dbd6b8bad7a2433eaff4c69d9be4
cyber-mat <olaniyanolamide@gmail.com>
2020-06-02 09:52:01 +0100

    App can now read value from user input

--- app.js
+++ app.js
@@ -1,33 +1,52 @@
-let budgetController = (function () {
-    let a = 23;
-
-    let add = function (x) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function (y) {
-            return add(y);
-        }
-    }
-})();
+let budgetController = (function () {})();
 
 let userController = (function () {
+    let DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
-    }
 
-)();
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
 
-let controller = (function (budgetCtrl, userCtrl) {
-
-        let z = budgetCtrl.publicTest(5);
-
-        return {
-            anotherPublic: function () {
-                console.log(z);
-            }
+        getDOMstrings: function () {
+            return DOMstrings;
         }
 
+    };
+})();
+
+let appController = (function (budgetCtrl, userCtrl) {
+
+    let DOM = userCtrl.getDOMstrings();
+
+    let ctrlAddItem = function () {
+        //1. Get the field input data
+
+        let input = userCtrl.getInput();
+        console.log(input);
+
+
+        //2. Add the item to the budget controller
+        //3. Add the item to UI
+        //4. Calculate the budget
+        //5. Display the budget on the UI
     }
 
-)(budgetController, userController);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (e) {
+        if (e.keyCode === 13 || e.which === 13) {
+            ctrlAddItem();
+        }
+    });
+})(budgetController, userController);
